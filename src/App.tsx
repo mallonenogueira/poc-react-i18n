9170f935f5e30831c4fa1ea309a798ccbb88f943
Mallone <mallonenogueira@gmail.com>
2021-10-22 00:00:21 -0300

    feat: create translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,41 @@
-function App() {
+import { ChangeEvent } from "react";
+import { useTranslation } from "react-i18next";
+
+function ChooseLanguage() {
+  const { t, i18n } = useTranslation();
+
+  function handleChangeLang(event: ChangeEvent<HTMLSelectElement>) {
+    i18n.changeLanguage(event.target.value);
+  }
+
   return (
     <div>
-      <h1>Meu titulo</h1>
+      <label htmlFor="choose-language">{t("labels.lang")}</label>
+
+      <select
+        id="choose-language"
+        value={i18n.language}
+        onChange={handleChangeLang}
+      >
+        <option value="pt-BR">{t("languages.pt")}</option>
+
+        <option value="en">{t("languages.en")}</option>
+      </select>
+    </div>
+  );
+}
+
+function App() {
+  const { t } = useTranslation();
+  const { t: pageTranslate } = useTranslation("pages/app");
+
+  return (
+    <div>
+      <h1>{t("titles.app", { title: "React i18n" })}</h1>
+
+      <h2>{pageTranslate("title")}</h2>
+
+      <ChooseLanguage />
     </div>
   );
 }
