6af9944a642f3578a5318a34af27f9c6d54cfe43
Mallone <mallonenogueira@gmail.com>
2021-10-22 00:32:45 -0300

    fix: change locales url

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -3,11 +3,17 @@ import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
+// console.log(window.ENV);
+console.log();
+
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
+    backend: {
+      loadPath: process.env.PUBLIC_URL + "/locales/{{lng}}/{{ns}}.json",
+    },
     fallbackLng: "en",
     debug: true,
     interpolation: {
